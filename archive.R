f0b353c963ee60369d6beae0accc0cd6768c7c66
DHatziioanou <DHatziioanou@users.noreply.github.com>
2020-10-06 10:32:49 +0100

    Allow archiving in other locations

--- archive.R
+++ archive.R
@@ -15,40 +15,49 @@
 #' @keywords archive
 #'
 #' @examples
-#' 
+#'
 #' archive("C:..../analysis")
-#' 
-#' 
+#'
+#'
 #' @export
 archive <- function(path, folder, date) {
-  if (!require("filesstrings")) {
-    install.packages("filesstrings")
-    library(filesstrings)
-  }
+
   # Archive folder name
   if (missing(folder)) {
     folder <- "Archive"
   }
-  
+
   # Date cut off
   if (missing(date)) {
     date <- Sys.Date()
   }
+
+  # Destination type 
+  destination <- ifelse(length(str_split(folder, pattern = "/")[[1]])>1, "path", "subfolder")
   
-  # Make Archive folder
-  if (!(dir.exists(file.path(path, folder)))) { 
-    dir.create(file.path(path, folder))
-  }  
-  
+  # Archive folder present
+  if(destination == "path"){
+    if (!(dir.exists(folder))) {
+     dir.create(file.path(folder))
+    }
+  } else if (destination == "subfolder" & !(dir.exists(file.path(path, folder)))) {
+     dir.create(file.path(path, folder))
+  }
+
   # Files to archive
   files <- list.files(path = path, recursive = F)
   files <- as.data.frame(files)
   files$files <- as.character(files$files)
   files$Modified <- file.info(file.path(path, files[,1]))$ctime
   files$paths  <- file.path(path, files$file)
-  files <- files[files$Modified < date & 
+  files <- files[files$Modified < date &
                    !(files$paths %in% list.dirs(path, recursive = F)),]
-  # Move files 
-  filesstrings::file.move(files = files$paths, destinations = file.path(path,folder), overwrite = TRUE)
-  
-}  
+
+  # Move files
+  ifelse(destination == "path", 
+         file.copy(from = files$paths, to = folder, overwrite = TRUE, recursive = TRUE,
+                   copy.mode = TRUE, copy.date = TRUE),
+         file.copy(from = files$paths, to = file.path(path,folder), overwrite = TRUE, recursive = TRUE,
+                   copy.mode = TRUE, copy.date = TRUE))
+  file.remove(files$paths)
+}
